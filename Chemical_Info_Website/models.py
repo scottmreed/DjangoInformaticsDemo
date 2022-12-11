34adbb02532506ca9779fca4f8a2e2311bada15d
scottmreed <53488644+scottmreed@users.noreply.github.com>
2022-12-10 21:01:22 -0700

    Creating model

--- Chemical_Info_Website/models.py
+++ Chemical_Info_Website/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Chemical(models.Model):
+    name = models.CharField(max_length=200)
+    BP_C = models.FloatField
+    BP_K = models.FloatField
+    smiles = models.CharField(max_length=200)
+    MW = models.FloatField
