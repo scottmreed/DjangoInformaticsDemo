f7d566c0220611c34b5835ab2928c01509b2aa0f
scottmreed <53488644+scottmreed@users.noreply.github.com>
2022-12-11 22:06:10 -0700

    Three straightforward views to start

--- Chemical_Info_Website/models.py
+++ Chemical_Info_Website/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Chemical(models.Model):
     name = models.CharField(max_length=200)
-    BP_C = models.FloatField
-    BP_K = models.FloatField
-    smiles = models.CharField(max_length=200)
-    MW = models.FloatField
+    BP_C = models.CharField(max_length=200, default=0)
+    BP_K = models.CharField(max_length=200, default=0)
+    smiles = models.CharField(max_length=200, default=0)
+    MW = models.CharField(max_length=200, default=0)
--- Chemical_Info_Website/urls.py
+++ Chemical_Info_Website/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from Chemical_Info_Website.views import index, chemical_list, chemical_addition
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Chemical_Info_Website.urls'), name='index'),
+    path('Chemical_Info_Website/smiles/<str:smiles>/', chemical_list, name='BP_get'),
+    path('Chemical_Info_Website/add_chemical', chemical_addition, name='adding_chem'),
+    path('Chemical_Info_Website/chemweb', index, name='index'),
 ]
